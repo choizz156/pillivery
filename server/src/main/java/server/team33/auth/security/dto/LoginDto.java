2f8c6c886f08b75731a61518ab9b825c4137fa7f
cohi <tkfka156@gmail.com>
2023-03-22 21:01:37 +0900

    :sparkles: refactor: LoginDto 리팩토링

--- server/src/main/java/server/team33/auth/security/dto/LoginDto.java
+++ server/src/main/java/server/team33/auth/security/dto/LoginDto.java
@@ -1,15 +1,19 @@
-package server.team33.login.dto;
+package server.team33.auth.security.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
 public class LoginDto {
+
     private String username;
     private String password;
+
+    @Builder
+    public LoginDto(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
