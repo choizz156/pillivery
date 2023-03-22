47094eb098d28f45acb32b320c166793fc69a376
cohi <tkfka156@gmail.com>
2023-03-22 20:52:06 +0900

    :sparkles: refactor: embadable 설정

---
+++ server/src/main/java/server/team33/user/entity/Address.java
@@ -0,0 +1,18 @@
+package server.team33.user.entity;
+
+import javax.persistence.Embeddable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Embeddable
+public class Address {
+    private String city;
+    private String detailAddress;
+
+    public Address(String city, String detailAddress) {
+        this.city = city;
+        this.detailAddress = detailAddress;
+    }
+}
