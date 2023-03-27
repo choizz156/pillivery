dd770e7d8b3cf79e9c0ab765375c5af7503453b0
cohi <tkfka156@gmail.com>
2023-03-27 12:25:23 +0900

    :recycle: refactor: 예외처리 리팩토링

--- server/src/main/java/server/team33/exception/controller/ExceptionController.java
+++ server/src/main/java/server/team33/exception/controller/ExceptionController.java
@@ -2,118 +2,57 @@ package server.team33.exception.controller;
 
 
 import io.jsonwebtoken.ExpiredJwtException;
+import javax.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.exception.DataException;
-import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.HttpClientErrorException;
 import server.team33.exception.bussiness.BusinessLogicException;
 import server.team33.exception.response.ErrorResponse;
-
-import javax.validation.ConstraintViolationException;
 
 @Slf4j
 @RestControllerAdvice
 public class ExceptionController {
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse methodArgumentNotValidException(
-            MethodArgumentNotValidException e ){
-       return ErrorResponse.of(e.getBindingResult());
-    }
-
-//    @ExceptionHandler
-//    public ResponseEntity<ErrorResponse> constraintViolationException(
-//            ConstraintViolationException e ){
-//        ErrorResponse response = ErrorResponse.of(e.getConstraintViolations());
-//
-//        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-//    }
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> businessLogicException( BusinessLogicException e ){
-        ErrorResponse response = ErrorResponse.of(e.getExceptionCode());
-        log.error("비지니스 예외 처리");
-        return new ResponseEntity<>(response, HttpStatus.valueOf(e.getExceptionCode().getCode()));
+    public ErrorResponse methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+        return ErrorResponse.of(e.getBindingResult());
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> httpRequestMethodNotSupportedException(
-            HttpRequestMethodNotSupportedException e ){
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse constraintViolationExceptionHandler(ConstraintViolationException ex) {
+        return ErrorResponse.of(ex);
+    }
 
-        ErrorResponse response = ErrorResponse.of(HttpStatus.METHOD_NOT_ALLOWED);
-
-        return new ResponseEntity<>(response, HttpStatus.METHOD_NOT_ALLOWED);
+    @ExceptionHandler(BusinessLogicException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse businessLogicExceptionHandler(BusinessLogicException e) {
+        return ErrorResponse.of(e.getMessage());
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> httpMessageNotReadableException(
-            HttpMessageNotReadableException e ){
-
-        ErrorResponse response = ErrorResponse.of(HttpStatus.BAD_REQUEST);
-
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse methodArgumentTypeMismatchExceptionHandler(
+        MethodArgumentNotValidException e) {
+        return ErrorResponse.of(e.getBindingResult());
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> missingServletRequestParameterException(
-            MissingServletRequestParameterException e ){
-
-        ErrorResponse response = ErrorResponse.of(HttpStatus.BAD_REQUEST, e.getMessage());
-
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse missingServletRequestParameterExceptionHandler(
+        MissingServletRequestParameterException e) {
+        return ErrorResponse.of(e.getMessage());
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> internalAuthenticationException(
-            InternalAuthenticationServiceException e ){
-
-        ErrorResponse response = ErrorResponse.of(HttpStatus.BAD_REQUEST, e.getMessage());
-
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse expiredJwtException(ExpiredJwtException e) {
+       return ErrorResponse.of( e.getMessage());
     }
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> expiredJwtException(
-            ExpiredJwtException e ){
-
-        ErrorResponse response = ErrorResponse.of(HttpStatus.NOT_IMPLEMENTED, e.getMessage());
-
-        return new ResponseEntity<>(response, HttpStatus.NOT_IMPLEMENTED);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> incorrectResultSizeDataAccessException(
-            IncorrectResultSizeDataAccessException e ){
-        ErrorResponse response = ErrorResponse.of(HttpStatus.NOT_ACCEPTABLE, e.getMessage());
-
-        return new ResponseEntity<>(response, HttpStatus.NOT_ACCEPTABLE);
-    }
-  @ExceptionHandler
-    public ResponseEntity<ErrorResponse> HttpClientErrorException(
-            HttpClientErrorException e ){
-        ErrorResponse response = ErrorResponse.of(HttpStatus.NOT_ACCEPTABLE, e.getResponseBodyAsString());
-
-        return new ResponseEntity<>(response, HttpStatus.NOT_ACCEPTABLE);
-    }
-
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> dataException(
-            DataException e ){
-
-        ErrorResponse response = ErrorResponse.of(HttpStatus.BAD_REQUEST, e.getMessage());
-
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
-
 }
+
--- server/src/main/java/server/team33/exception/response/ErrorResponse.java
+++ server/src/main/java/server/team33/exception/response/ErrorResponse.java
@@ -1,15 +1,14 @@
 package server.team33.exception.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.validation.ConstraintViolationException;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import server.team33.exception.bussiness.ExceptionCode;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 @JsonInclude(value = JsonInclude.Include.NON_EMPTY)
@@ -18,7 +17,6 @@ public class ErrorResponse {
     private final int status;
     private final String message;
     private final List<CustomFieldError> customFieldErrors;
-//    private List<ConstraintViolationError> violationErrors;
 
     @Builder
     public ErrorResponse(int status, String message, List<CustomFieldError> customFieldErrors) {
@@ -27,67 +25,54 @@ public class ErrorResponse {
         this.customFieldErrors = customFieldErrors;
     }
 
-    public static ErrorResponse of( BindingResult bindingResult ){
-        return ErrorResponse.builder().customFieldErrors(CustomFieldError.of(bindingResult)).build();
+    public static ErrorResponse of(BindingResult bindingResult) {
+        return ErrorResponse.builder().customFieldErrors(CustomFieldError.of(bindingResult))
+            .build();
     }
 
-//    public static ErrorResponse of( Set<ConstraintViolation<?>> violations ){
-//        return ErrorResponse.builder().violationErrors(ConstraintViolationError.of(violations)).build();
-//    }
-
-    public static ErrorResponse of( ExceptionCode exceptionCode ){// 비지니스 예외처리 할 때 사용
-            return ErrorResponse.builder().status(exceptionCode.getCode()).message(exceptionCode.getMessage()).build();
+    public static ErrorResponse of(ExceptionCode exceptionCode) {
+        return ErrorResponse.builder().status(exceptionCode.getCode())
+            .message(exceptionCode.getMessage()).build();
     }
 
-    public static ErrorResponse of( HttpStatus httpStatus ){//저장되어 있는 문구
-            return ErrorResponse.builder().status(httpStatus.value()).message(httpStatus.getReasonPhrase()).build();
+    public static ErrorResponse of(HttpStatus httpStatus) {//저장되어 있는 문구
+        return ErrorResponse.builder().status(httpStatus.value())
+            .message(httpStatus.getReasonPhrase()).build();
     }
 
-    public static ErrorResponse of( HttpStatus httpStatus, String message ){ //직접 쓰는 문장
-        return ErrorResponse.builder().status(httpStatus.value()).message(message).build();
+    public static ErrorResponse of(ConstraintViolationException e) {
+        List<CustomFieldError> errors = e.getConstraintViolations().stream()
+            .map(v -> new CustomFieldError(v.getPropertyPath().toString(),
+                null,
+                v.getMessage()))
+            .collect(Collectors.toList());
+        return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage(), errors);
+    }
+
+    public static ErrorResponse of(String message) { //직접 쓰는 문장
+        return ErrorResponse.builder().message(message).build();
     }
 
     @Getter
     public static class CustomFieldError {
+
         private String field;
         private Object rejectedValue;
         private String reason;
 
-        private CustomFieldError( String field, Object rejectedValue, String reason ){
+        private CustomFieldError(String field, Object rejectedValue, String reason) {
             this.field = field;
             this.rejectedValue = rejectedValue;
             this.reason = reason;
         }
 
-        public static List<CustomFieldError> of( BindingResult bindingResult ){
+        public static List<CustomFieldError> of(BindingResult bindingResult) {
             final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
             return fieldErrors.stream()
-                    .map(error -> new CustomFieldError(error.getField(),
-                                                 error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
-                                                 error.getDefaultMessage()))
-                    .collect(Collectors.toList());
+                .map(error -> new CustomFieldError(error.getField(),
+                    error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
+                    error.getDefaultMessage()))
+                .collect(Collectors.toList());
         }
     }
-
-//    @Getter
-//    public static class ConstraintViolationError {
-//        private String propertyPath;
-//        private Object rejectedValue;
-//        private String reason;
-//
-//        private ConstraintViolationError( String propertyPath, Object rejectedValue, String reason ){
-//            this.propertyPath = propertyPath;
-//            this.rejectedValue = rejectedValue;
-//            this.reason = reason;
-//        }
-//
-//        public static List<ConstraintViolationError> of( Set<ConstraintViolation<?>> constraintViolations ){
-//            return constraintViolations
-//                    .stream()
-//                    .map(constraintViolation -> new ConstraintViolationError(constraintViolation.getPropertyPath().toString(),
-//                                                                             constraintViolation.getInvalidValue().toString(),
-//                                                                             constraintViolation.getMessage()))
-//                    .collect(Collectors.toList());
-//        }
-//    }
 }
