0e3156de0a4e5f1f536ab0d14a01de42a823a61e
cohi <tkfka156@gmail.com>
2023-07-03 11:43:35 +0900

    :recycle: refactor: 트리거 api 수정

--- server/src/main/java/server/team33/domain/subscription/trigger/TriggerService.java
+++ server/src/main/java/server/team33/domain/subscription/trigger/TriggerService.java
@@ -1,31 +1,28 @@
 package server.team33.domain.subscription.trigger;
 
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+import java.util.Date;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.JobKey;
 import org.quartz.Trigger;
 import org.springframework.stereotype.Component;
 import server.team33.domain.order.entity.ItemOrder;
 
-import java.util.Date;
-
-import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
-import static org.quartz.TriggerBuilder.newTrigger;
-
 @Slf4j
 @Component
 public class TriggerService {
-//TODO 메서드 3개가 다 똑같네... 나중에 하나로 통일
-    public Trigger buildTrigger( JobKey jobKey, Long orderId, ItemOrder itemOrder ){
 
-        return  newTrigger()
-                 .forJob(jobKey)
-                 .withSchedule(simpleSchedule()
-                                  .withIntervalInMinutes(itemOrder.getPeriod())
-                                  .repeatForever()
+    public Trigger build(JobKey jobKey, ItemOrder itemOrder) {
+
+        return newTrigger()
+            .forJob(jobKey)
+            .withSchedule(simpleSchedule()
+                .withIntervalInMinutes(itemOrder.getPeriod())
+                .repeatForever()
             )
-//                .startNow()
-                .startAt(Date.from(itemOrder.getNextDelivery().toInstant())) //TODO 정석
-                .build();
+            .startAt(Date.from(itemOrder.getNextDelivery().toInstant()))
+            .build();
     }
-
 }
