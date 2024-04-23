4d8aca6f2af2c8ed9e8a62ce7e90fd44bbc3cdc2
choizz <tkfka156@gmail.com>
2024-04-23 21:05:25 +0900

    test: 쿼리 정렬 테스트 추가

--- server/module-core/src/test/java/com/team33/modulecore/order/application/OrderQueryServiceTest.java
+++ server/module-core/src/test/java/com/team33/modulecore/order/application/OrderQueryServiceTest.java
@@ -7,6 +7,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.team33.modulecore.item.domain.Item;
 import com.team33.modulecore.order.domain.Order;
 import com.team33.modulecore.order.domain.OrderStatus;
+import com.team33.modulecore.order.dto.OrderPageRequest;
 import com.team33.modulecore.orderitem.domain.OrderItem;
 import com.team33.modulecore.orderitem.domain.OrderItemInfo;
 import com.team33.modulecore.user.domain.User;
@@ -31,36 +32,59 @@ class OrderQueryServiceTest extends OrderDomainHelper {
     @Test
     void 주문_목록_조회() throws Exception {
         //given
-        User user = getUser();
-        List<Item> items = getItems();
-        List<OrderItemInfo> orderItemInfos = List.of(
+        var user = getUser();
+        var items = getItems();
+        var orderItemInfos1 = List.of(
             OrderItemInfo.of(3, false, 0),
             OrderItemInfo.of(2, false, 0),
             OrderItemInfo.of(1, false, 0)
         );
 
-        List<OrderItem> orderItems = List.of(
-            OrderItem.createWithoutOrder(items.get(0), orderItemInfos.get(0)),
-            OrderItem.createWithoutOrder(items.get(1), orderItemInfos.get(1)),
-            OrderItem.createWithoutOrder(items.get(2), orderItemInfos.get(2))
+        var orderItemInfoSubs = List.of(
+            OrderItemInfo.of(3, true,30),
+            OrderItemInfo.of(2, true, 30),
+            OrderItemInfo.of(1, true, 30 )
+        );
+
+        var orderItems = List.of(
+            OrderItem.createWithoutOrder(items.get(0), orderItemInfos1.get(0)),
+            OrderItem.createWithoutOrder(items.get(1), orderItemInfos1.get(1)),
+            OrderItem.createWithoutOrder(items.get(2), orderItemInfos1.get(2))
+        );
+        var orderItemsSubs = List.of(
+            OrderItem.createWithoutOrder(items.get(0), orderItemInfoSubs.get(0)),
+            OrderItem.createWithoutOrder(items.get(1), orderItemInfoSubs.get(1)),
+            OrderItem.createWithoutOrder(items.get(2), orderItemInfoSubs.get(2))
         );
 
         Order order = orderService.callOrder(orderItems, false, user.getId());
+        Order orderSubs = orderService.callOrder(orderItemsSubs, true, user.getId());
         orderService.completeOrder(order.getId());
+        orderService.subscribeOrder(orderSubs.getId());
 
+        var orderPageRequest = OrderPageRequest.of(1, 5);
         //when
-        Page<Order> allOrders = orderQueryService.findAllOrders(user.getId(), 1, 5, false);
+        Page<Order> allOrders = orderQueryService.findAllOrders(user.getId(), orderPageRequest);
+
 
         //then
         List<Order> content = allOrders.getContent();
-        assertThat(allOrders.getNumberOfElements()).isEqualTo(1);
-        assertThat(content).hasSize(1)
+        assertThat(allOrders.getSize()).isEqualTo(7);
+        assertThat(allOrders.getTotalPages()).isEqualTo(1);
+        assertThat(allOrders.getNumberOfElements()).isEqualTo(2);
+        assertThat(content).hasSize(2)
             .extracting("id", "user", "orderStatus")
-            .containsExactlyInAnyOrder(
+            .containsExactly(
+                tuple(orderSubs.getId(), user, OrderStatus.ORDER_SUBSCRIBE),
                 tuple(order.getId(), user, OrderStatus.ORDER_COMPLETE)
             );
 
         assertThat(content.get(0).getOrderItems())
+            .hasSize(3)
+            .extracting("item")
+            .containsExactlyInAnyOrder(items.get(0), items.get(1), items.get(2));
+
+        assertThat(content.get(1).getOrderItems())
             .hasSize(3)
             .extracting("item")
             .containsExactlyInAnyOrder(items.get(0), items.get(1), items.get(2));
