2079ca898bb1e02d85deb7f4400ce437e0a3d220
choizz <tkfka156@gmail.com>
2024-06-17 11:17:52 +0900

    test: JobDetailServiceTest 테스트 작성

---
+++ server/module-quartz/src/test/java/com/team33/modulequartz/subscription/application/JobDetailServiceTest.java
@@ -0,0 +1,29 @@
+package com.team33.modulequartz.subscription.application;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.quartz.JobDetail;
+import org.quartz.JobKey;
+
+class JobDetailServiceTest {
+
+	@DisplayName("jobDetail을 생성할 수 있다.")
+	@Test
+	void jobDetail_생성() throws Exception {
+		//given
+		JobKey jobKey = JobKeyGenerator.build(1L, "product");
+
+		//when
+		JobDetailService jobDetailService = new JobDetailService();
+		JobDetail jobDetail = jobDetailService.build(jobKey, 1L);
+
+		//then
+		assertThat(jobDetail.getKey().getName()).isEqualTo("1-product");
+		assertThat(jobDetail.getKey().getGroup()).isEqualTo("1");
+		assertThat(jobDetail.getJobDataMap()).hasSize(2)
+			.extracting("orderId", "retry")
+			.contains(1L, 0);
+	}
+}
