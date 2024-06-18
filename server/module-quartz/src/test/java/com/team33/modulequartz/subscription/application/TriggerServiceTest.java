2de07e7a25619253e90d64951ce7d87f947a7844
choizz <tkfka156@gmail.com>
2024-06-18 21:24:41 +0900

    test(TriggerServiceTest) : trigger 테스트 작성

---
+++ server/module-quartz/src/test/java/com/team33/modulequartz/subscription/application/TriggerServiceTest.java
@@ -0,0 +1,38 @@
+package com.team33.modulequartz.subscription.application;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.quartz.JobKey;
+import org.quartz.Trigger;
+
+import com.team33.modulecore.order.domain.OrderItem;
+import com.team33.modulecore.order.domain.SubscriptionInfo;
+
+class TriggerServiceTest {
+
+	@DisplayName("trigger를 생성할 수 있다.")
+	@Test
+	void trigger_생성() throws Exception{
+		//given
+		TriggerService triggerService = new TriggerService();
+
+		OrderItem orderItem = OrderItem.builder()
+			.subscriptionInfo(SubscriptionInfo.of(true, 30))
+			.build();
+
+		//when
+		Trigger trigger = triggerService.build(new JobKey("1", "1"), orderItem);
+
+		//then
+		assertThat(trigger.getKey().getName()).isEqualTo("1");
+		assertThat(trigger.getKey().getGroup()).isEqualTo("1");
+		assertThat(trigger.getStartTime()).isEqualTo(Date.from(orderItem.getNextDelivery().toInstant()));
+		assertThat(trigger.getFireTimeAfter(trigger.getStartTime()))
+			.isEqualTo(Date.from(orderItem.getSubscriptionInfo().getNextDelivery().plusDays(30).toInstant()));
+	}
+
+}
