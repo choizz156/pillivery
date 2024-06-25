f91abce9c55cbac0c346aa2e22207727d948127c
choizz <tkfka156@gmail.com>
2024-06-25 12:11:19 +0900

    rename(PaymentJobListeners): 네이밍 변경

--- server/module-quartz/src/main/java/com/team33/modulequartz/subscription/application/SubscriptionService.java
+++ server/module-quartz/src/main/java/com/team33/modulequartz/subscription/application/SubscriptionService.java
@@ -16,7 +16,7 @@ import com.team33.modulecore.order.application.OrderQueryService;
 import com.team33.modulecore.order.application.OrderStatusService;
 import com.team33.modulecore.order.domain.OrderItem;
 import com.team33.modulecore.order.domain.entity.Order;
-import com.team33.modulequartz.subscription.infra.JobListeners;
+import com.team33.modulequartz.subscription.infra.PaymentJobListeners;
 import com.team33.modulequartz.subscription.infra.TriggerListeners;
 
 import lombok.RequiredArgsConstructor;
@@ -165,7 +165,7 @@ public class SubscriptionService {
 		try {
 			ListenerManager listenerManager = scheduler.getListenerManager();
 			listenerManager.addJobListener(
-				new JobListeners(
+				new PaymentJobListeners(
 					trigger,
 					orderItemService,
 					orderCreateService,
