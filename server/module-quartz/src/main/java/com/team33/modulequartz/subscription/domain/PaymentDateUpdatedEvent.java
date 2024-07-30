9aeb2ddd03513101d1a47b44a0cf9ef53d72ef8f
choizz <tkfka156@gmail.com>
2024-07-30 18:00:08 +0900

    refactor(PaymentDateUpdatedEvent): 결제일 필드 추가

--- server/module-quartz/src/main/java/com/team33/modulequartz/subscription/domain/PaymentDateUpdatedEvent.java
+++ server/module-quartz/src/main/java/com/team33/modulequartz/subscription/domain/PaymentDateUpdatedEvent.java
@@ -1,5 +1,8 @@
 package com.team33.modulequartz.subscription.domain;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 import com.team33.modulecore.order.domain.OrderItem;
 
 import lombok.Getter;
@@ -8,6 +11,7 @@ import lombok.Getter;
 public class PaymentDateUpdatedEvent {
 
 	private final OrderItem orderItem;
+	private final ZonedDateTime paymentDay = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
 
 	public PaymentDateUpdatedEvent(OrderItem orderItem) {
 		this.orderItem = orderItem;
