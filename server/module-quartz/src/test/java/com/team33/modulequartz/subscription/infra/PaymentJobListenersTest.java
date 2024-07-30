134ba6914cf4195202d9559d2ebe790af5ec29c7
choizz <tkfka156@gmail.com>
2024-07-30 17:17:49 +0900

    test: job 재시도 후 취소 로직 테스트

--- server/module-quartz/src/test/java/com/team33/modulequartz/subscription/infra/PaymentJobListenersTest.java
+++ server/module-quartz/src/test/java/com/team33/modulequartz/subscription/infra/PaymentJobListenersTest.java
@@ -9,21 +9,27 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.quartz.Job;
 import org.quartz.JobDataMap;
 import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.JobKey;
 import org.quartz.ListenerManager;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.Trigger;
 import org.quartz.impl.StdSchedulerFactory;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.context.ApplicationEventPublisher;
 
+import com.team33.modulecore.exception.BusinessLogicException;
 import com.team33.modulecore.order.domain.OrderItem;
 import com.team33.modulequartz.subscription.domain.PaymentDateUpdatedEvent;
 
+@ExtendWith(OutputCaptureExtension.class)
 class PaymentJobListenersTest {
 
 	private Scheduler scheduler;
@@ -61,10 +67,10 @@ class PaymentJobListenersTest {
 
 	@DisplayName("job 수행이 한 번 실패시 바로 재시도한다.")
 	@Test
-	void job_수행_실패() throws Exception {
+	void job_수행_실패(CapturedOutput output) throws Exception {
 		// given
 		ApplicationEventPublisher mockPublisher = mock(ApplicationEventPublisher.class);
-		PaymentJobListeners listeners = new PaymentJobListeners(mockPublisher, null);
+		PaymentJobListeners listeners = new PaymentJobListeners(mockPublisher);
 
 		JobExecutionContext mockContext = mock(JobExecutionContext.class);
 		JobExecutionException mockException = mock(JobExecutionException.class);
@@ -79,8 +85,40 @@ class PaymentJobListenersTest {
 
 		// then
 		verify(mockException).setRefireImmediately(true);
-		verify(mockPublisher,times(1)).publishEvent(any(PaymentDateUpdatedEvent.class));
+		verify(mockPublisher, times(1)).publishEvent(any(PaymentDateUpdatedEvent.class));
+
 		assertThat(jobDataMap.getInt("retry")).isEqualTo(1);
+		assertThat(output).contains("job exception = " + mockException.getMessage());
+	}
+
+	@DisplayName("job 수행이 두 번 실패시 스케쥴이 취소된다.")
+	@Test
+	void job_수행_실패_2(CapturedOutput output) throws Exception {
+		// given
+		ApplicationEventPublisher mockPublisher = mock(ApplicationEventPublisher.class);
+		PaymentJobListeners listeners = new PaymentJobListeners(mockPublisher);
+
+		JobExecutionContext mockContext = mock(JobExecutionContext.class);
+		JobExecutionException mockException = mock(JobExecutionException.class);
+
+		JobDataMap jobDataMap = new JobDataMap();
+		jobDataMap.put("retry", 2);
+		JobDetail jobDetail = mock(JobDetail.class);
+		when(jobDetail.getKey()).thenReturn(new JobKey("testKey"));
+		when(mockContext.getJobDetail()).thenReturn(jobDetail);
+		when(mockContext.getJobDetail().getJobDataMap()).thenReturn(jobDataMap);
+
+		// when then
+		assertThatThrownBy(
+			() -> listeners.jobWasExecuted(mockContext, mockException)
+		)
+			.isInstanceOf(BusinessLogicException.class);
+
+		verify(mockException).setUnscheduleAllTriggers(true);
+		verify(mockPublisher, times(0)).publishEvent(any(PaymentDateUpdatedEvent.class));
+
+		assertThat(jobDataMap.getInt("retry")).isEqualTo(2);
+		assertThat(output).contains(" job 예외로 인한 스케쥴 취소 = ");
 	}
 
 	private ApplicationEventPublisher getMockApplicationEventPublisher() {
@@ -90,7 +128,7 @@ class PaymentJobListenersTest {
 	}
 
 	private void addJobListeners(ApplicationEventPublisher applicationContext) throws SchedulerException {
-		PaymentJobListeners paymentJobListeners = new PaymentJobListeners(applicationContext, null);
+		PaymentJobListeners paymentJobListeners = new PaymentJobListeners(applicationContext);
 		ListenerManager listenerManager = scheduler.getListenerManager();
 		listenerManager.addJobListener(paymentJobListeners);
 	}
