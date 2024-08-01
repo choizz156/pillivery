9b39703b36c391add3f9f7e0b8c5906d8afa331d
choizz <tkfka156@gmail.com>
2024-08-01 12:02:38 +0900

    refactor(EventApiForwarder): registerEventWithDispatcher 로직 변경

--- server/module-event/src/main/java/com/team33/moduleevent/application/EventApiForwarder.java
+++ server/module-event/src/main/java/com/team33/moduleevent/application/EventApiForwarder.java
@@ -1,6 +1,8 @@
 package com.team33.moduleevent.application;
 
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -11,18 +13,39 @@ import com.team33.moduleevent.domain.EventType;
 import com.team33.moduleevent.domain.entity.ApiEvent;
 import com.team33.moduleevent.domain.repository.EventRepository;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RequiredArgsConstructor
 @Component
 public class EventApiForwarder {
 
 	private final EventRepository eventsRepository;
 	private final EventSender kakaoApiEventSender;
 	private final EventSender scheduleRegisterEventSender;
+	private final EventSender scheduleCancelEventSender;
 	private final EventDispatcher eventDispatcher;
+	private final Map<EventType, EventSender> eventTypeEventSenderMap;
+
+	public EventApiForwarder(
+		EventRepository eventsRepository,
+		EventSender kakaoApiEventSender,
+		EventSender scheduleRegisterEventSender,
+		EventSender scheduleCancelEventSender,
+		EventDispatcher eventDispatcher
+	) {
+		this.eventsRepository = eventsRepository;
+		this.kakaoApiEventSender = kakaoApiEventSender;
+		this.scheduleRegisterEventSender = scheduleRegisterEventSender;
+		this.scheduleCancelEventSender = scheduleCancelEventSender;
+		this.eventDispatcher = eventDispatcher;
+
+		eventTypeEventSenderMap = new EnumMap<>(EventType.class);
+		eventTypeEventSenderMap.put(EventType.KAKAO_REFUNDED, kakaoApiEventSender);
+		eventTypeEventSenderMap.put(EventType.SCHEDULE_REGISTERED, scheduleRegisterEventSender);
+		eventTypeEventSenderMap.put(EventType.SCHEDULE_CANCELED, scheduleCancelEventSender);
+		eventTypeEventSenderMap.put(EventType.SUBS_CANCELED,kakaoApiEventSender);
+
+	}
 
 	@Transactional
 	@Scheduled(cron = "0 * * * * *")
@@ -42,14 +65,16 @@ public class EventApiForwarder {
 	}
 
 	private void registerEventWithDispatcher(ApiEvent apiEvent) {
-		if (isScheduleRegistered(apiEvent)) {
-			eventDispatcher.register(apiEvent, scheduleRegisterEventSender);
-			return;
-		}
-		eventDispatcher.register(apiEvent, kakaoApiEventSender);
+		EventType type = apiEvent.getType();
+		EventSender eventSender = eventTypeEventSenderMap.getOrDefault(type, kakaoApiEventSender);
+		eventDispatcher.register(apiEvent, eventSender);
 	}
 
-	private boolean isScheduleRegistered(ApiEvent apiEvent) {
-		return apiEvent.getType().equals(EventType.SCHEDULE_REGISTERED.name());
+	private boolean isScheduleCancelEvent(String apiEvent) {
+		return apiEvent.equals(EventType.SCHEDULE_CANCELED.name());
+	}
+
+	private boolean isScheduleRegisterEvent(String apiEvent) {
+		return apiEvent.equals(EventType.SCHEDULE_REGISTERED.name());
 	}
 }
