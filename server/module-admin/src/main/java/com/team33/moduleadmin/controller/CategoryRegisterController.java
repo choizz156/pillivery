1f751239cdfd49118681712dac56d92d80638bc3
choizz <tkfka156@gmail.com>
2024-08-06 16:21:21 +0900

    fix: 카테고리 추가 로직 수정

--- server/module-admin/src/main/java/com/team33/moduleadmin/controller/CategoryRegisterController.java
+++ server/module-admin/src/main/java/com/team33/moduleadmin/controller/CategoryRegisterController.java
@@ -1,9 +1,11 @@
 package com.team33.moduleadmin.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +16,13 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(value = "/categories", method = RequestMethod.POST)
+@RequestMapping(value = "/categories")
 public class CategoryRegisterController {
 
+	private static final Logger log = LoggerFactory.getLogger(CategoryRegisterController.class);
 	private final CategoryRegisterService categoryRegisterService;
 
+	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public void registerCategory(@RequestBody CategoryPostDto postDto) {
 		categoryRegisterService.save(postDto.getCategoryName());
