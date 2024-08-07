a917f6e5cd3bac39ad11f26a9c264d316f7ccfcf
choizz <tkfka156@gmail.com>
2024-08-07 17:06:59 +0900

    test: 카테고리 캐싱 테스트 작성

--- server/module-core/src/test/java/com/team33/modulecore/cache/CacheClientTest.java
+++ server/module-core/src/test/java/com/team33/modulecore/cache/CacheClientTest.java
@@ -1,9 +1,11 @@
 package com.team33.modulecore.cache;
 
+import static com.team33.modulecore.core.category.domain.CategoryName.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.DisplayName;
@@ -12,15 +14,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.team33.modulecore.FixtureMonkeyFactory;
+import com.team33.modulecore.cache.dto.CachedCategoryItems;
+import com.team33.modulecore.cache.dto.CachedMainItems;
 import com.team33.modulecore.config.RedisTestConfig;
+import com.team33.modulecore.core.category.domain.Categories;
+import com.team33.modulecore.core.category.domain.CategoryName;
 import com.team33.modulecore.core.item.domain.repository.ItemQueryRepository;
+import com.team33.modulecore.core.item.dto.query.ItemPage;
 import com.team33.modulecore.core.item.dto.query.ItemQueryDto;
+import com.team33.modulecore.core.item.dto.query.PriceFilter;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @ActiveProfiles("test")
@@ -98,5 +110,77 @@ class CacheClientTest {
 			.contains("test");
 
 		verify(itemQueryRepository, times(0)).findItemsWithDiscountRateMain();
+	}
+
+	@DisplayName("카테고리별 아이템들이 캐싱되지 않았을 경우 캐싱한다. 8개")
+	@Test
+	void 카테고리_캐싱_x() throws Exception {
+		//given
+		List<ItemQueryDto> itemQueryDtos = FixtureMonkeyFactory.get().giveMeBuilder(ItemQueryDto.class)
+			.set("categories", new Categories(Set.of(EYE)))
+			.sampleList(8);
+
+		Page<ItemQueryDto> page = PageableExecutionUtils.getPage(
+			itemQueryDtos,
+			PageRequest.of(0, 8),
+			() -> 100
+		);
+
+		//given
+		ItemQueryRepository itemQueryRepository = mock(ItemQueryRepository.class);
+		when(itemQueryRepository.findItemsByCategory(any(CategoryName.class), eq(""), any(PriceFilter.class), any(ItemPage.class)))
+			.thenReturn(page);
+
+		CacheClient cacheClient = new CacheClient(redisTemplate, itemQueryRepository);
+
+		//when
+		CachedCategoryItems<ItemQueryDto> categoryItems =
+			cacheClient.getCategoryItems(EYE, "", new PriceFilter(), new ItemPage());
+
+		//then
+		List<ItemQueryDto> content = categoryItems.getContent();
+		ValueOperations<String, Object> ops = redisTemplate.opsForValue();
+		Long expireTime = redisTemplate.getExpire("EYE", TimeUnit.DAYS); //남은 만료시간
+
+		assertThat(expireTime).isEqualTo(2L);
+		assertThat(ops.get("EYE")).isInstanceOf(CachedCategoryItems.class);
+		assertThat(content).hasSize(8).doesNotContainNull();
+	}
+
+	@DisplayName("카테고리별 아이템들이 캐싱되어 있을 경우 db를 거치지 않는다.")
+	@Test
+	void 카테고리_캐싱_o() throws Exception {
+		//given
+		List<ItemQueryDto> itemQueryDtos = FixtureMonkeyFactory.get().giveMeBuilder(ItemQueryDto.class)
+			.set("categories", new Categories(Set.of(EYE)))
+			.sampleList(8);
+
+		Page<ItemQueryDto> page = PageableExecutionUtils.getPage(
+			itemQueryDtos,
+			PageRequest.of(0, 8),
+			() -> 100
+		);
+
+		//given
+		ItemQueryRepository itemQueryRepository = mock(ItemQueryRepository.class);
+		when(itemQueryRepository.findItemsByCategory(any(CategoryName.class), eq(""), any(PriceFilter.class), any(ItemPage.class)))
+			.thenReturn(page);
+
+		CacheClient cacheClient = new CacheClient(redisTemplate, itemQueryRepository);
+
+		//when
+		CachedCategoryItems<ItemQueryDto> categoryItems =
+			cacheClient.getCategoryItems(EYE, "", new PriceFilter(), new ItemPage());
+
+		//then
+		List<ItemQueryDto> content = categoryItems.getContent();
+		ValueOperations<String, Object> ops = redisTemplate.opsForValue();
+		Long expireTime = redisTemplate.getExpire("EYE", TimeUnit.DAYS); //남은 만료시간
+
+		assertThat(expireTime).isEqualTo(2L);
+		assertThat(ops.get("EYE")).isInstanceOf(CachedCategoryItems.class);
+		assertThat(content).hasSize(8).doesNotContainNull();
+
+		verify(itemQueryRepository, times(0)).findItemsByCategory(any(CategoryName.class), eq(""), any(PriceFilter.class), any(ItemPage.class));
 	}
 }
