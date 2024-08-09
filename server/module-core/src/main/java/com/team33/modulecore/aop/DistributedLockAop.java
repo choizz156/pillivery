4bdcb03877b6597b6d94acbc1f6462ea300989fd
choizz <tkfka156@gmail.com>
2024-08-09 23:28:23 +0900

    feat: 분산 락 redisson으로 변경 -> RedLock 사용

--- server/module-core/src/main/java/com/team33/modulecore/aop/DistributedLockAop.java
+++ server/module-core/src/main/java/com/team33/modulecore/aop/DistributedLockAop.java
@@ -5,8 +5,9 @@ import java.util.concurrent.TimeUnit;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
 import com.team33.modulecore.core.item.domain.entity.Item;
@@ -21,39 +22,36 @@ import lombok.extern.slf4j.Slf4j;
 public class DistributedLockAop {
 
 	private final RedisTemplate<String, Object> redisTemplate;
+	private final RedissonClient redissonClient;
 
 	@Around("execution(* com.team33.modulecore.core.item.application.ItemStarService.*StarAvg(..))")
 	public Object distributedLock(ProceedingJoinPoint joinPoint) {
 		Item arg = (Item)joinPoint.getArgs()[0];
 		String key = "item Id lock :" + arg.getId();
-		ValueOperations<String, Object> ops = redisTemplate.opsForValue();
 
-		Boolean hasLock = false;
-		long start = System.currentTimeMillis();
-		long retrySecond = 3000L;
-
-		while (!hasLock && System.currentTimeMillis() - start < retrySecond) {
-			hasLock = ops.setIfAbsent(key, "true", 10L, TimeUnit.SECONDS);
-
-			if (!hasLock) {
-				try {
-					Thread.sleep(100L);
-				} catch (InterruptedException e) {
-					throw new RuntimeException(e);
-				}
-			}
-		}
-
-		if (!hasLock) {
-			throw new RuntimeException("락을 획득할 수 없습니다.");
-		}
+		RLock rLock = redissonClient.getLock(key);
 
 		try {
+			boolean hasLock = rLock.tryLock(3L, 5L, TimeUnit.SECONDS);
+
+			if (!hasLock) {
+				log.warn("락 획득 실패 : {}", key);
+				return false;
+			}
+
 			return joinPoint.proceed();
 		} catch (Throwable e) {
-			throw new RuntimeException(e);
+			throw new RuntimeException(e.getMessage());
 		} finally {
-			redisTemplate.delete(key);
+			unlock(rLock, key);
+		}
+	}
+
+	private void unlock(RLock rLock, String key) {
+		try {
+			rLock.unlock();
+		} catch (IllegalMonitorStateException e) {
+			log.warn("lock failed : {}, key : {}", e.getMessage(), key);
 		}
 	}
 }
--- server/module-core/src/main/java/com/team33/modulecore/config/RedisConfig.java
+++ server/module-core/src/main/java/com/team33/modulecore/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.team33.modulecore.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +24,18 @@ public class RedisConfig {
 	@Value("${spring.data.redis.port}")
 	private int port;
 
+
+	private static final String REDISSON_HOST_PREFIX = "redis://";
+
+	@Bean
+	public RedissonClient redissonClient() {
+		RedissonClient redissonClient = null;
+		Config config = new Config();
+		config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + host + ":" + port);
+		redissonClient = Redisson.create(config);
+		return redissonClient;
+	}
+
 	@Bean
 	public LettuceConnectionFactory lettuceConnectionFactory() {
 
@@ -34,7 +49,7 @@ public class RedisConfig {
 
 	@Bean
 	public RedisTemplate<?,?> redisTemplate() {
-		RedisTemplate<?,?> redisTemplate = new RedisTemplate<>();
+		RedisTemplate<?,?> redisTemplate = new RedisTemplate<>(){};
 		redisTemplate.setConnectionFactory(lettuceConnectionFactory());
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
 		redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
