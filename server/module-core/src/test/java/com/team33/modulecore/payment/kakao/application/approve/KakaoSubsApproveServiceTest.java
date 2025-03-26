4e11b486811eafca35dcd8729e36a8a4ffa3dc09
choizz <tkfka156@gmail.com>
2025-03-26 21:27:03 +0900

    test: 결제일 업데이트 이벤트 발행 로직 테스트

--- server/module-core/src/test/java/com/team33/modulecore/payment/kakao/application/approve/KakaoSubsApproveServiceTest.java
+++ server/module-core/src/test/java/com/team33/modulecore/payment/kakao/application/approve/KakaoSubsApproveServiceTest.java
@@ -1,11 +1,14 @@
 package com.team33.modulecore.payment.kakao.application.approve;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationEventPublisher;
 
 import com.team33.modulecore.FixtureMonkeyFactory;
 import com.team33.modulecore.core.order.domain.OrderPrice;
@@ -15,6 +18,7 @@ import com.team33.modulecore.core.payment.kakao.application.ParameterProvider;
 import com.team33.modulecore.core.payment.kakao.application.approve.KakaoFirstSubsApprove;
 import com.team33.modulecore.core.payment.kakao.application.approve.KakaoSubsApprove;
 import com.team33.modulecore.core.payment.kakao.application.approve.KakaoSubsApproveService;
+import com.team33.modulecore.core.payment.kakao.application.events.PaymentDateUpdatedEvent;
 import com.team33.modulecore.core.payment.kakao.dto.KakaoApproveOneTimeRequest;
 import com.team33.modulecore.core.payment.kakao.dto.KakaoApproveResponse;
 import com.team33.moduleexternalapi.dto.kakao.KakaoApiApproveResponse;
@@ -23,15 +27,11 @@ class KakaoSubsApproveServiceTest {
 
 	@DisplayName("첫 정기 결제 승인을 위임할 수 있다.")
 	@Test
-	void 첫_정기_승인_위임() throws Exception{
+	void 첫_정기_승인_위임() throws Exception {
 		//given
-		ParameterProvider parameterProvider = new ParameterProvider();
-
-		KakaoSubsApproveService kakaoSubsApproveService = new KakaoSubsApproveService(
-			applicationEventPublisher -> {},
-			new KakaoFirstSubsApprove((params, url) -> new KakaoApiApproveResponse(), parameterProvider),
-			new KakaoSubsApprove((params, url) -> new KakaoApiApproveResponse(), parameterProvider)
-		);
+		KakaoSubsApproveService kakaoSubsApproveService = getKakaoSubsApproveService(
+			applicationEventPublisher -> {
+			});
 
 		ApproveRequest request = KakaoApproveOneTimeRequest.builder()
 			.orderId(1L)
@@ -48,16 +48,12 @@ class KakaoSubsApproveServiceTest {
 
 	@DisplayName("기존 정기 결제 승인을 위임할 수 있다.")
 	@Test
-	void 정기_승인_위임() throws Exception{
+	void 정기_승인_위임() throws Exception {
 
 		//given
-		ParameterProvider parameterProvider = new ParameterProvider();
-
-		KakaoSubsApproveService kakaoSubsApproveService = new KakaoSubsApproveService(
-			applicationEventPublisher -> {},
-			new KakaoFirstSubsApprove((params, url) -> new KakaoApiApproveResponse(), parameterProvider),
-			new KakaoSubsApprove((params, url) -> new KakaoApiApproveResponse(), parameterProvider)
-		);
+		KakaoSubsApproveService kakaoSubsApproveService = getKakaoSubsApproveService(
+			applicationEventPublisher -> {
+			});
 
 		Order order = FixtureMonkeyFactory.get().giveMeBuilder(Order.class)
 			.set("id", 1L)
@@ -67,7 +63,7 @@ class KakaoSubsApproveServiceTest {
 			.set("orderPrice", new OrderPrice(3000, 200))
 			.set("orderItems", List.of())
 			.set("receiver", null)
-			.set("sid", "sid")
+			.set("paymentId.sid", "sid")
 			.set("orderItems", null)
 			.set("user", null)
 			.sample();
@@ -77,6 +73,74 @@ class KakaoSubsApproveServiceTest {
 
 		//then
 		assertThat(kaKaoApiApproveResponse).isNotNull();
+	}
+
+	@DisplayName("첫 정기 결제 승인 후 결제일을 업데이트 이벤트를 발행한다.")
+	@Test
+	void test2() throws Exception {
+		//given
+		ApplicationEventPublisher applicationContext = getApplicationEventPublisher();
+
+		KakaoSubsApproveService kakaoSubsApproveService = getKakaoSubsApproveService(applicationContext);
+
+		ApproveRequest request = KakaoApproveOneTimeRequest.builder()
+			.orderId(1L)
+			.pgtoken("pgToken")
+			.tid("tid")
+			.build();
+
+		//when
+		kakaoSubsApproveService.approveFirstTime(request);
+
+		//then
+		verify(applicationContext, times(1)).publishEvent(any(PaymentDateUpdatedEvent.class));
+
+	}
+
+	@DisplayName("기존 정기 결제 승인시 결제 날짜 업데이트 이벤트를 발행한다.")
+	@Test
+	void test3() throws Exception {
+
+		//given
+		ApplicationEventPublisher applicationContext = getApplicationEventPublisher();
+
+		KakaoSubsApproveService kakaoSubsApproveService = getKakaoSubsApproveService(applicationContext);
+
+		Order order = FixtureMonkeyFactory.get().giveMeBuilder(Order.class)
+			.set("id", 1L)
+			.set("mainItemName", "test")
+			.set("totalItemsCount", 3)
+			.set("totalQuantity", 3)
+			.set("orderPrice", new OrderPrice(3000, 200))
+			.set("orderItems", List.of())
+			.set("receiver", null)
+			.set("paymentId.sid", "sid")
+			.set("orderItems", null)
+			.set("user", null)
+			.sample();
+
+		//when
+		KakaoApproveResponse kaKaoApiApproveResponse = kakaoSubsApproveService.approveSubscribe(order);
+
+		//then
+		verify(applicationContext, times(1)).publishEvent(any(PaymentDateUpdatedEvent.class));
+	}
+
+	private KakaoSubsApproveService getKakaoSubsApproveService(ApplicationEventPublisher applicationContext) {
+		ParameterProvider parameterProvider = new ParameterProvider();
+
+		KakaoSubsApproveService kakaoSubsApproveService = new KakaoSubsApproveService(
+			applicationContext,
+			new KakaoFirstSubsApprove((params, url) -> new KakaoApiApproveResponse(), parameterProvider),
+			new KakaoSubsApprove((params, url) -> new KakaoApiApproveResponse(), parameterProvider)
+		);
+		return kakaoSubsApproveService;
+	}
+
+	private ApplicationEventPublisher getApplicationEventPublisher() {
+		ApplicationEventPublisher applicationContext = mock(ApplicationEventPublisher.class);
+		doNothing().when(applicationContext).publishEvent(any(PaymentDateUpdatedEvent.class));
+		return applicationContext;
 	}
 
 }
