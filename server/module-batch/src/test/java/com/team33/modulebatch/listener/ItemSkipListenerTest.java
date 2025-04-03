60dd571a1faa856a4a789824f22670ad19bbc20e
choizz <tkfka156@gmail.com>
2025-04-03 10:57:06 +0900

    test: ItemSkipListenerTest 추가

---
+++ server/module-batch/src/test/java/com/team33/modulebatch/listener/ItemSkipListenerTest.java
@@ -0,0 +1,43 @@
+package com.team33.modulebatch.listener;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.team33.modulebatch.domain.ErrorItemRepository;
+import com.team33.modulebatch.domain.entity.ErrorItem;
+import com.team33.modulebatch.step.SubscriptionOrderVO;
+
+@ExtendWith(MockitoExtension.class)
+class ItemSkipListenerTest {
+
+    @Mock
+    private ErrorItemRepository errorItemRepository;
+
+    @DisplayName("결제 실패 시 에러 아이템을 저장한다")
+    @Test
+    void test1() {
+        // given
+        ItemSkipListener listener = new ItemSkipListener(errorItemRepository);
+        
+        SubscriptionOrderVO orderVO = new SubscriptionOrderVO();
+        orderVO.setIdempotencyKey("test-key");
+        
+        RuntimeException exception = new RuntimeException("결제 실패");
+
+        // when
+        listener.onSkipInWrite(orderVO, exception);
+
+        // then
+        ArgumentCaptor<ErrorItem> captor = ArgumentCaptor.forClass(ErrorItem.class);
+        verify(errorItemRepository, times(1)).save(captor.capture());
+        
+        ErrorItem savedItem = captor.getValue();
+        verify(errorItemRepository).save(savedItem);
+    }
+}
