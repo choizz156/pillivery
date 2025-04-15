9cac82bb27c7349b4e47b3a1874b9c72e97bc46f
choizz <tkfka156@gmail.com>
2025-04-16 02:28:05 +0900

    fix: 카트 조회 쿼리 수정

--- server/module-core/src/main/java/com/team33/modulecore/core/cart/domain/repository/CartRepository.java
+++ server/module-core/src/main/java/com/team33/modulecore/core/cart/domain/repository/CartRepository.java
@@ -3,12 +3,18 @@ package com.team33.modulecore.core.cart.domain.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.team33.modulecore.core.cart.domain.entity.CartEntity;
 import com.team33.modulecore.core.cart.domain.entity.NormalCartEntity;
 import com.team33.modulecore.core.cart.domain.entity.SubscriptionCartEntity;
 
 public interface CartRepository extends JpaRepository<CartEntity, Long> {
-	Optional<NormalCartEntity> findNormalCartById(Long cartId);
-	Optional<SubscriptionCartEntity> findSubscriptionCartById(Long cartId);
+
+	@Query("SELECT c FROM CartEntity c WHERE c.id = :cartId AND TYPE(c) = NormalCartEntity")
+	Optional<NormalCartEntity> findNormalCartById(@Param("cartId") Long cartId);
+
+	@Query("SELECT c FROM CartEntity c WHERE c.id = :cartId AND TYPE(c) = SubscriptionCartEntity")
+	Optional<SubscriptionCartEntity> findSubscriptionCartById(@Param("cartId") Long cartId);
 }
