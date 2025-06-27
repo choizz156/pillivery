31c6c82fa5a01ca5d31ff53f20d7ddc7627bf954
choizz156 <tkfka156@gmail.com>
2025-06-27 21:20:26 +0900

    refactor: 벌크헤드 설정으로 인한 WebClientConfig 변경

--- server/module-external-api/src/main/java/com/team33/moduleexternalapi/config/WebClientConfig.java
+++ server/module-external-api/src/main/java/com/team33/moduleexternalapi/config/WebClientConfig.java
@@ -1,56 +1,39 @@
 package com.team33.moduleexternalapi.config;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import io.netty.channel.ChannelOption;
-import io.netty.handler.timeout.ReadTimeoutHandler;
-import io.netty.handler.timeout.WriteTimeoutHandler;
-import lombok.RequiredArgsConstructor;
 import reactor.netty.http.client.HttpClient;
-import reactor.netty.resources.ConnectionProvider;
-import reactor.netty.resources.LoopResources;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebClientConfig {
 
 
-	public static final String PAYMENT_API = "payment-api-";
-
 	private final WebClientErrorHandler errorHandler;
 
 	@Bean
 	public WebClient webClient() {
 
-		int optimalThreadCount = Runtime.getRuntime().availableProcessors() * 2;
-
-		LoopResources loopResources = LoopResources.create(PAYMENT_API, optimalThreadCount, false);
-
-		ConnectionProvider provider = ConnectionProvider.builder("external-api-pool")
-				.maxConnections(50)//vuser = 100 -> 50
-				.pendingAcquireTimeout(Duration.ofSeconds(3))
-				.evictInBackground(Duration.ofSeconds(30))
-				.build();
-
-		HttpClient httpClient = HttpClient.create(provider)
-				.runOn(loopResources)
-				.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
-				.responseTimeout(Duration.ofSeconds(30))
-				.doOnConnected(conn ->
-					conn.addHandlerLast(new ReadTimeoutHandler(25, TimeUnit.SECONDS))
-						.addHandlerLast(new WriteTimeoutHandler(25, TimeUnit.SECONDS))
-				);
+		HttpClient httpClient = HttpClient.create()
+			.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
+			.responseTimeout(Duration.ofSeconds(30))
+			.doOnConnected(conn ->
+				conn.addHandlerLast(new ReadTimeoutHandler(25, TimeUnit.SECONDS))
+					.addHandlerLast(new WriteTimeoutHandler(25, TimeUnit.SECONDS))
+			);
 
 		return WebClient.builder()
-				.clientConnector(new ReactorClientHttpConnector(httpClient))
-				.filter(errorHandler.errorResponseFilter())
-				.filter(errorHandler.networkExceptionFilter())
-				.build();
+			.clientConnector(new ReactorClientHttpConnector(httpClient))
+			.filter(errorHandler.errorResponseFilter())
+			.filter(errorHandler.networkExceptionFilter())
+			.build();
 	}
 }
